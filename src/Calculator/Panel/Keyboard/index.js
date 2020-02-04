9f96e89cd7e4b6375345e0d7dd8ec1df9c0138a2
OffS3c <jk.offs3c@gmail.com>
2020-02-04 14:32:38 +0400

    added dynamic buttons

--- src/Calculator/Panel/Keyboard/index.js
+++ src/Calculator/Panel/Keyboard/index.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Button from "./Button";
+import Board from "./board";
+// import '../index.css';
 
 const keyboard = () => {
   return (
-    <div>
+    <div className="c-calculator__body js-calculator__body">
+      {Board.map((row, i) =>
+        row.map((col, j) => <Button data={col} key={j} />)
+      )}
     </div>
   );
 };
